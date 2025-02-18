ec14a0c94eee22356b809b40e4634bc7ef50b402
Mathias Kriebel <mathias.kriebel@gmail.com>
2025-02-17 23:11:08 -0300

    feat: save webhook creation time

--- WebhookTesterAPI/Models/Webhook.cs
+++ WebhookTesterAPI/Models/Webhook.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public Guid OwnerToken { get; set; } = Guid.Empty;
+        public DateTimeOffset Created { get; set; } = DateTimeOffset.UtcNow;
         public List<WebhookRequest> Requests { get; set; } = new();
     }
 
